b6810c1a7bd17593d519e4073f138f0ce6d0b9d6
ashrtonomica <as.shtronomica@gmail.com>
2026-08-20 13:01:48 +0530

    Update links.ts

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -2,7 +2,7 @@ export const PDF_VIEWER_URL =
   "https://cdn.jsdelivr.net/gh/0xDarkSeidBull/daotask8@main/task08.pdf";
 
 export const DOCS_VIEWER_URL =
-  "https://docs.google.com/viewer?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2F0xDarkSeidBull%2Fdaotask8%40main%2Ftask08.docx&embedded=true";
+  "https://docs.google.com/viewer?url=https://raw.githubusercontent.com/0xDarkSeidBull/daotask8/main/task08.docx&embedded=true";
 
 export const ARTICLE_URL =
   "https://dev.to/0xdarkseidbull/how-to-bridge-eth-from-ethereum-sepolia-to-redbelly-network-testnet-a-complete-lock-and-mint-4i88";
